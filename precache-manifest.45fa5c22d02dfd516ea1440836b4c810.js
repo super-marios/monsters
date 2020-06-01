65c3d499ee506e639896aa4baf60e5acf0fce365
wootangg <wjckorea001@gmail.com>
2020-06-01 23:45:46 +0900

    Updates

---
+++ precache-manifest.45fa5c22d02dfd516ea1440836b4c810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b002f3f2aca46689a554152e792eb6f",
+    "url": "/github.io/mosters/index.html"
+  },
+  {
+    "revision": "cb7a433eb47ed014274c",
+    "url": "/github.io/mosters/static/css/main.0aa81541.chunk.css"
+  },
+  {
+    "revision": "08d115c20f93248ad510",
+    "url": "/github.io/mosters/static/js/2.25c969ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/mosters/static/js/2.25c969ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7a433eb47ed014274c",
+    "url": "/github.io/mosters/static/js/main.32237002.chunk.js"
+  },
+  {
+    "revision": "d8de0f5fc580e819a32e",
+    "url": "/github.io/mosters/static/js/runtime-main.9413a20b.js"
+  }
+]);
